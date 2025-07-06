2845d81109f1e61e1cf2baea9f112b606122edba
Ram-S5 <smetanin.rom@yandex.ru>
2025-07-06 20:17:14 +0300

    практическое 6

---
+++ 6_pr/js/script.js
@@ -0,0 +1,27 @@
+const viewImg = document.querySelector("#view-img")
+const img1 = document.querySelector("#img-1")
+const img2 = document.querySelector("#img-2")
+const img3 = document.querySelector("#img-3")
+const img4 = document.querySelector("#img-4")
+const img5 = document.querySelector("#img-5")
+const img6 = document.querySelector("#img-6")
+
+
+img1.addEventListener("click", ()=>{
+viewImg.src=img1.src
+})
+img2.addEventListener("click", ()=>{
+viewImg.src=img2.src
+})
+img3.addEventListener("click", ()=>{
+viewImg.src=img3.src
+})
+img4.addEventListener("click", ()=>{
+viewImg.src=img4.src
+})
+img5.addEventListener("click", ()=>{
+viewImg.src=img5.src
+})
+img6.addEventListener("click", ()=>{
+viewImg.src=img6.src
+})
