165bba247aa88eea85927de1a414e326799a1f6c
Ram-S5 <smetanin.rom@yandex.ru>
2025-07-15 16:39:05 +0300

    Итоговая+

--- THE_FINAL_TASK/js/index.js
+++ THE_FINAL_TASK/js/index.js
@@ -1,17 +1,14 @@
-const burgerBtnMenu = document.querySelector("#burger-btn");
-const burgerMenu = document.querySelector("#burger-menu");
-const burgerBtnMenuClose = document.querySelector("#burger-btn-menu-close");
+const burgerBtnMenu = document.querySelector("#burger-btn")
+const burgerMenu = document.querySelector("#burger-menu")
+const burgerBtnMenuClose = document.querySelector("#burger-btn-menu-close")
 
 burgerBtnMenu.addEventListener('click', () => {
     burgerMenu.style.left = "0px"
 })
 
 burgerBtnMenuClose.addEventListener("click", () => {
-   burgerMenu.style.left = "-400px";
+   burgerMenu.style.left = "-400px"
 })
-
-
-
 
 
 const dirCards = document.querySelectorAll(".direction-card")
