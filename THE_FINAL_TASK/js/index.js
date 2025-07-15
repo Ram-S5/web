b5c4353aaf6c705ed81a228669c249c776544eb6
Ram-S5 <smetanin.rom@yandex.ru>
2025-07-15 15:10:28 +0300

    ashgsbnt

--- THE_FINAL_TASK/js/index.js
+++ THE_FINAL_TASK/js/index.js
@@ -1,24 +1,16 @@
-// const burgerBtnMenu = document.querySelector("#burger-btn-menu");
-// const burgerMenu = document.querySelector(".header-nav");
-// const burgerBtnMenuClose = document.querySelector("#burger-btn-menu-close");
+const burgerBtnMenu = document.querySelector("#burger-btn");
+const burgerMenu = document.querySelector("#burger-menu");
+const burgerBtnMenuClose = document.querySelector("#burger-btn-menu-close");
 
-// burgerBtnMenu.addEventListener('click', () => {
-//     burgerMenu.style.right = "0px"
-// })
+burgerBtnMenu.addEventListener('click', () => {
+    burgerMenu.style.left = "0px"
+})
 
-// burgerBtnMenuClose.addEventListener("click", () => {
-//    burgerMenu.style.right = "-100px";
-// })
+burgerBtnMenuClose.addEventListener("click", () => {
+   burgerMenu.style.left = "-400px";
+})
 
 
-// const burger = document.querySelector('.burger');
-
-// burger.addEventListener('click', () => {
-//     burger.classList.toggle('active');
-//     if (burger.classList.contains('active')) { burgerMenu.style.right = "0px"; }
-//     else { burgerMenu.style.right = "-200px"; }
-
-// });
 
 
 
