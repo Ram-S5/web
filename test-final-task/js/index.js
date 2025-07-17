599b56c2dfe6b337c49d7f1f84f6a62d0c2389c6
Ram-S5 <smetanin.rom@yandex.ru>
2025-07-17 09:48:16 +0300

    Исправил язык в хэде и поставил спаны в буттонах

---
+++ test-final-task/js/index.js
@@ -0,0 +1,26 @@
+const burgerBtnMenu = document.querySelector("#burger-btn")
+const burgerMenu = document.querySelector("#burger-menu")
+const burgerBtnMenuClose = document.querySelector("#burger-btn-menu-close")
+
+burgerBtnMenu.addEventListener('click', () => {
+    burgerMenu.style.left = "0px"
+})
+
+burgerBtnMenuClose.addEventListener("click", () => {
+   burgerMenu.style.left = "-400px"
+})
+
+
+const dirCards = document.querySelectorAll(".direction-card")
+const buttons = document.querySelectorAll(".buttons button")
+
+buttons.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        buttons.forEach((btn) => btn.style.backgroundColor = "")
+        btn.style.backgroundColor = "var(--accent-color)"
+        for (card of dirCards) {
+            card.style.left = -index * 120 + "%"
+
+        }
+    })
+})
